a96fbfa19c4a9083ba3e6f61df867777a5732887
sxq <sxq@97kid.com>
2016-08-19 10:56:00 +0800

    1

--- controllers/serverAddr.go
+++ controllers/serverAddr.go
@@ -9,9 +9,9 @@ func HandlerServerAddr(r *gin.Engine) {
 		data := make(gin.H)
 
 		data["name"] = "game"
-		data["ls_ip"] = "139.129.33.151"
+		data["ls_host"] = "139.129.33.151"
 		data["ls_port"] = 7777
-		data["gs_ip"] = "139.129.33.151"
+		data["gs_host"] = "139.129.33.151"
 		data["gs_port"] = 8888
 
 		c.JSON(200, data)
